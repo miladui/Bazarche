479113bc374dbc800da81b41c2871c8a0a8d72ca
mohammad <mohammadsilver2030@gmail.com>
2024-12-29 14:57:46 +0330

    clean code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type {Metadata} from "next";
 import "./globals.css";
 import Header from "./../components/header";
 import Footer from "@/components/footer";
-import Login from "@/components/login";
 
 export const metadata: Metadata = {
     title: "Create Next App",
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Image from 'next/image';
-import 'font-awesome/css/font-awesome.min.css';
 
 
 const Footer = () => {
